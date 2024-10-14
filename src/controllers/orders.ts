f67342c4f92c24c27558c52ccc0015812aa796cd
Abhay <abhaysharma.developer@gmail.com>
2024-10-15 00:24:24 +0530

    Sell order

--- src/controllers/orders.ts
+++ src/controllers/orders.ts
@@ -1,22 +1,81 @@
 import { Request, Response } from "express";
-import { YES_ORDER_REQUEST } from "../interfaces";
-import { INR_BALANCES } from "../config/globals";
+import { INR_BALANCES, ORDERBOOK, STOCK_BALANCES } from "../config/globals";
+import { ORDER_REQUEST } from "../interfaces/requestModels";
 
+// Get order book
+export const getOrderBook = (req: Request, res: Response) => {
+  res.send({ data: ORDERBOOK });
+};
+
+// Buy Order
 export const buyOrder = (req: Request, res: Response) => {
-  const { userId, stockSymbol, quantity, price }: YES_ORDER_REQUEST = req.body;
-
-  const requiredBalance = quantity * price;
-  const userBalance = INR_BALANCES[`user${userId}`].balance;
-
-  if (requiredBalance > userBalance) {
-    return res.send({ message: "Insufficient Balance" });
-  }
+  res.send({ message: "Order placed successfully" });
 };
 
+// Sell Order
 export const sellOrder = (req: Request, res: Response) => {
-  console.log("Sell order");
-};
+  const { userId, stockSymbol } = req.body as ORDER_REQUEST;
+  const quantity = Number(req.body.quantity);
+  const price = Number(req.body.price);
+  const stockType = req.body.stockType as "yes" | "no";
 
-export const viewOrders = (req: Request, res: Response) => {
-  console.log("View order");
+  const userExists = INR_BALANCES[userId];
+  const symbolExists = ORDERBOOK[stockSymbol];
+
+  if (!userExists) {
+    res.send({ error: `User with user Id ${userId} does not exist` });
+    return;
+  }
+  if (!symbolExists) {
+    res.send({ error: `Stock with stockSymbol ${stockSymbol} does not exist` });
+    return;
+  }
+
+  const stockAvailable = STOCK_BALANCES[userId][stockSymbol]; // Does user already have this stock.
+
+  if (!stockAvailable) {
+    res.send({ message: `You do not own any stock of ${stockSymbol}` });
+    return;
+  }
+
+  const stockBalanceOfUser = Number(stockAvailable[stockType]?.quantity) || 0; // Quantity of stocks user own
+  const lockedStocksOfUser = Number(stockAvailable[stockType]?.locked) || 0; // Quantity of Locked stocks
+
+  if (quantity > stockBalanceOfUser) {
+    res.send({ message: "Insufficient Stocks" });
+    return;
+  }
+
+  const priceInRupee = price / 100;
+  const prevQuantityInOrderBook =
+    Number(ORDERBOOK[stockSymbol][stockType]?.[priceInRupee]?.total) || 0;
+
+  // Subtracting from user stock quantity and adding to locked stocks
+  stockAvailable[stockType] = {
+    quantity: stockBalanceOfUser - quantity,
+    locked: lockedStocksOfUser + quantity,
+  };
+
+  // Adding to order book
+  if (prevQuantityInOrderBook === 0) {
+    const orderData = {
+      total: quantity,
+      orders: {
+        [userId]: quantity,
+      },
+    };
+    ORDERBOOK[stockSymbol][stockType][priceInRupee] = orderData;
+  } else {
+    ORDERBOOK[stockSymbol][stockType][priceInRupee].total =
+      prevQuantityInOrderBook + quantity;
+
+    const prevUserStockCount =
+      Number(ORDERBOOK[stockSymbol][stockType][priceInRupee].orders[userId]) ||
+      0;
+
+    ORDERBOOK[stockSymbol][stockType][priceInRupee].orders[userId] =
+      prevUserStockCount + quantity;
+  }
+
+  res.send({ message: "Order placed successfully" });
 };
