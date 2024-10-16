0d0c05d93ce6e574aaceaab21bf97c5cb5846ae3
Abhay <abhaysharma.developer@gmail.com>
2024-10-17 00:46:12 +0530

    orderbook schema change

--- src/config/globals.ts
+++ src/config/globals.ts
@@ -5,26 +5,53 @@ import {
 } from "../interfaces/globals";
 
 export const INR_BALANCES: INR_BALANCES_TYPE = {
-  user2: {
-    balance: 1000000,
+  user1: {
+    balance: 100000,
     locked: 0,
   },
-  user1: {
-    balance: 0,
+  user2: {
+    balance: 100000,
+    locked: 0,
+  },
+  user3: {
+    balance: 100000,
+    locked: 0,
+  },
+  user4: {
+    balance: 100000,
+    locked: 0,
+  },
+  userr5: {
+    balance: 100000,
     locked: 0,
   },
 };
 
 export const STOCK_BALANCES: STOCK_BALANCES_TYPE = {
-  user2: {},
   user1: {
     ETH_USD_15_Oct_2024_12_00: {
       yes: {
-        quantity: 943,
-        locked: 57,
+        quantity: 0,
+        locked: 0,
       },
       no: {
-        quantity: 1000,
+        quantity: 0,
+        locked: 0,
+      },
+    },
+  },
+  user2: {
+    ETH_USD_15_Oct_2024_12_00: {
+      no: {
+        quantity: 0,
+        locked: 0,
+      },
+    },
+  },
+  user3: {
+    ETH_USD_15_Oct_2024_12_00: {
+      yes: {
+        quantity: 0,
         locked: 0,
       },
     },
@@ -33,20 +60,31 @@ export const STOCK_BALANCES: STOCK_BALANCES_TYPE = {
 
 export let ORDERBOOK: ORDER_BOOK_TYPE = {
   ETH_USD_15_Oct_2024_12_00: {
-    yes: {
-      "140": {
-        total: 20,
+    yes: [
+      {
+        price: 9.0,
+        total: 110,
         orders: {
-          user1: 20,
+          user1: [
+            { id: 123432213, quantity: 20, type: "buy" },
+            { id: 123243213, quantity: 20, type: "sell" },
+            { id: 123211113, quantity: 20, type: "sell" },
+          ],
+          user2: [
+            { id: 123213222, quantity: 10, type: "sell" },
+            { id: 123211213, quantity: 25, type: "sell" },
+            { id: 123213334, quantity: 15, type: "sell" },
+          ],
         },
       },
-      "150": {
+      {
+        price: 8.0,
         total: 20,
         orders: {
-          user1: 20,
+          user1: [{ id: 123234321, quantity: 20, type: "sell" }],
         },
       },
-    },
-    no: {},
+    ],
+    no: [],
   },
 };
--- src/interfaces/globals.ts
+++ src/interfaces/globals.ts
@@ -20,18 +20,41 @@ export interface STOCK_BALANCES_TYPE {
   };
 }
 
+export type priceRange =
+  | 0
+  | 0.5
+  | 1
+  | 1.5
+  | 2
+  | 2.5
+  | 3
+  | 3.5
+  | 4
+  | 4.5
+  | 5
+  | 5.5
+  | 6
+  | 6.5
+  | 7
+  | 7.5
+  | 8
+  | 8.5
+  | 9
+  | 9.5
+  | 10;
+
 export type ORDERDATA = {
-  [price: number]: {
-    total: number;
-    orders: {
-      [userId: string]: number;
-    };
+  price: priceRange;
+  total: number;
+  orders: {
+    [userId: string]: {
+      id: number;
+      quantity: number;
+      type: string;
+    }[];
   };
 };
 
 export interface ORDER_BOOK_TYPE {
-  [stockSymbol: string]: {
-    yes: ORDERDATA;
-    no: ORDERDATA;
-  };
+  [stockSymbol: string]: { yes: ORDERDATA[]; no: ORDERDATA[] };
 }
